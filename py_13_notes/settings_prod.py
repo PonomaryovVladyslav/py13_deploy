38f716edc9be0c5dc5af2e74e4d30f2976417642
Vladyslav Ponomarov <blackbirdvlad@gmail.com>
2025-07-13 14:03:32 +0200

    add URL

--- py_13_notes/settings_prod.py
+++ py_13_notes/settings_prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'i^m-t@4p9rb%pr_m1*qv$3zb%3$y5iyp$^ay1nodp(r8g1e*81'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['3.120.147.160']
+ALLOWED_HOSTS = ['3.120.147.160', 'a-level-deploy.com']
 
 
 DATABASES = {
