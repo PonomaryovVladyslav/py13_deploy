acef638d791620b47b8616e207424be2743320e1
Vladyslav Ponomarov <blackbirdvlad@gmail.com>
2025-07-13 14:27:21 +0200

    add s3 desc

--- py_13_notes/settings_prod.py
+++ py_13_notes/settings_prod.py
@@ -28,8 +28,7 @@ AWS_STORAGE_BUCKET_NAME = 'py13deploy'
 AWS_S3_REGION_NAME = 'us-east-1'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET')
-# НЕ ВПИСЫВАЙТЕ САМИ КЛЮЧИ, ТОЛЬКО os.environ.get('SOME_KEY')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
-# you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
